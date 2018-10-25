938e6b9a595b857b57843b58b32c9a2c5ce74864
ffanzhang <ffanzhang@hotmail.com>
2018-10-25 00:50:07 -0700

    a trick picked up from an interview

--- flashcard/flashcard.py
+++ flashcard/flashcard.py
@@ -7,33 +7,36 @@ import time
 
 # interrupt handler for Ctrl + C, for exiting
 def exit(signum, frame):
-	print("\nExiting")
-	sys.exit(0)
-
+    print("\nExiting")
+    sys.exit(0)
 
 def main():
-	signal.siginterrupt(signal.SIGINT, True)
-	signal.signal(signal.SIGINT, exit)
-	args = sys.argv
-	if len(args) != 2 or args[1][-4:] == 'help':
-		print("usage: python flashcard.py filename.json")
-		sys.exit(0)
+    signal.siginterrupt(signal.SIGINT, True)
+    signal.signal(signal.SIGINT, exit)
+    args = sys.argv
+    if len(args) != 2 or args[1][-4:] == 'help':
+        print("usage: python flashcard.py filename.json")
+        sys.exit(0)
 
-	with open(args[1], 'r') as f:
-		text = f.read()
-		d = json.loads(text)
-		while True:
-			os.system('clear')
-			print("Starting a new round")
-			print("Press enter to proceed")
-			input()
-			keys = list(d.keys())
-			random.shuffle(keys)
-			for key in keys:
-				os.system('clear')
-				print(key)
-				input()
-				print(d[key])
-				input()
+    with open(args[1], 'r') as f:
+         text = f.read()
+         d = json.loads(text)
+         keys = list(d.keys())
+         n = len(keys)
+         while True:
+             os.system('clear')
+             print("Starting a new round")
+             print("Press enter to proceed")
+             input()
+             for i in range(n):
+                 target = random.randrange(i, n)
+                 keys[i], keys[target] = keys[target], keys[i]
+                 koi = keys[i]
+                 os.system('clear')
+                 print(koi)
+                 input()
+                 print(d[koi])
+                 input()
+                 
 if __name__ == "__main__":
-	main()
+     main()
