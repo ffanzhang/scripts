8a9be403af58d41a14dc18d79392423d98352e7c
ffanzhang <ffanzhang@hotmail.com>
2018-10-28 23:03:07 -0700

    remove extra files

--- cf/utils.py
+++
@@ -1,8 +0,0 @@
-def guess_language_code(ext):
-    if ext == "cc" or ext == "cpp":
-        return "50"
-    elif ext == "java":
-        return "36"
-    elif ext == "py":
-        return "7"
-    return "50"
