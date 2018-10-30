fffe91c98f10cf0b59703f5551f167695daec19e
ffanzhang <ffanzhang@hotmail.com>
2018-10-30 01:24:05 -0700

    made console app a bit shorter

--- cf/cf.py
+++ cf/cf.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     parser.add_argument("-cfg", "--configure", help="Use a configuration file instead, this overrides all the other options.")
     parser.add_argument("-u", "--username", help="cf handle")
     parser.add_argument("-m", "--mode", default="p", help="optional, by default problem set, problem=p, gym=g, contest=c")
-    parser.add_argument("-l", "--language_code", default="50", help="cf language code if you know it")
+    parser.add_argument("-l", "--language_code", help="cf language code if you know it")
     parser.add_argument("-f", "--file_name", help="file name to be submitted")
     parser.add_argument("-i", "--id", help="cf problem set id, gym id or contest id")
     parser.add_argument("-x", "--index", help="cf problem index A/B/C/D/E")
@@ -39,22 +39,13 @@ if __name__ == "__main__":
         if last_user is not None and last_user != args.username:
             auth.clear_cookies()
 
-        if args.id is None:
-            print("Please specify a contest/virtual/problem id. (example: -i 1234)")
+        if args.id is None or args.index is None or args.file_name is None:
+            parser.print_help()
+            print("One or more of problem/gym/contest id, index or file name is missing")
             sys.exit(1)
         else:
             id = args.id
-
-        if args.index is None:
-            print("Please specify an index. (example: -x A)")
-            sys.exit(1)
-        else:
             index = args.index
-
-        if args.file_name is None:
-            print("Please specify a file name. (example: -f r2d2.cc)")
-            sys.exit(1)
-        else:
             file_name = args.file_name
             
         if args.language_code is None:
