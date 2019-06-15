b8828d6ef314269c3775ff07626ae9b60b292f1e
ffanzhang <ffanzhang@hotmail.com>
2019-06-14 22:20:19 -0700

    check if python source exists before making a test file

--- compile/pp.sh
+++ compile/pp.sh
@@ -2,8 +2,7 @@
 DIR="${BASH_SOURCE%/*}"
 source $DIR/create_test_file_if_missing.sh
 pp() {
-    create_test_file_if_missing $1
-    test -f $1.py && time python $1.py < $1.in;
+    test -f $1.py && create_test_file_if_missing $1 && time python $1.py < $1.in;
 }
 
 pp $1
