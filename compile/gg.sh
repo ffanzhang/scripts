5fa38f4351b26fb25de3f099248f65af901f484d
Fan Zhang <ffanzhang@hotmail.com>
2019-06-15 23:15:18 -0700

    script to compile g++ for mac

--- compile/gg.sh
+++ compile/gg.sh
@@ -4,6 +4,8 @@ source $DIR/create_test_file_if_missing.sh
 gg() {
     test -f $1.cc && g++ -g -O2 -std=c++11 $1.cc -o $1 && create_test_file_if_missing $1 && time ./$1 < $1.in;
     test -f $1.cpp && g++ -g -O2 -std=c++11 $1.cpp -o $1 && create_test_file_if_missing $1 && time ./$1 < $1.in;
+    test `uname -s` == Darwin && test -f $1.cc && g++-9 -g -O2 -std=c++11 $1.cc -o $1 && create_test_file_if_missing $1 && ./$1 < $1.in;
+    test `uname -s` == Darwin && test -f $1.cpp && g++-9 -g -O2 -std=c++11 $1.cpp -o $1 && create_test_file_if_missing $1 && ./$1 < $1.in;
 }
 
 gg $1
