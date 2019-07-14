0a67a49ac02adfd0ae61ec0d6437d03a2bd2c850
ffanzhang <ffanzhang@hotmail.com>
2019-07-13 19:38:28 -0700

    remove echo

--- kf/kill_firefox_local.sh
+++ kf/kill_firefox_local.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 dest_folder=`find ~/.mozilla/firefox -type d -maxdepth 1 -name *default*`
-echo $dest_folder
-echo $dest_folder/lock
 if [ ${#dest_folder} -gt 0 ]; then
     rm $dest_folder/lock
     rm $dest_folder/.parentlock
